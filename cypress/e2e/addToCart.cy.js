c19650a76990eacd642ac6e2148cf80a196226c4
Laureano Boniel <laureano.boniel@legalmatch.com>
2025-06-18 15:16:24 +0800

    Select Random Item

--- cypress/e2e/addToCart.cy.js
+++ cypress/e2e/addToCart.cy.js
@@ -1,9 +1,9 @@
 import Login from '../pages/Login.js'
 import Inventory from '../pages/Inventory.js'
 
-describe('Login to SauceLab', () => {
+describe('Selecting Items and verify to Cart', () => {
 
-  it('Navigate to SauceLab', () => {
+  it('Add items to cart', () => {
     cy.visit('https://www.saucedemo.com/')
     Login.typeUsername()
     Login.typePassword()
--- cypress/pages/Inventory.js
+++ cypress/pages/Inventory.js
@@ -8,6 +8,17 @@ const Inventory = {
     },
     verifyNumberOfItems: function() {
         cy.get('.inventory_item').should('have.length', 6)
+    }, 
+    selectRandomItem: function() {
+        cy.get('[data-test="inventory-item"]').then(($items) => {
+            const max = $items.length;
+            const randomNumber = Math.floor(Math.random() * (max - 1) + 1);
+            cy.log(`Random number of items to add: ${randomNumber}`);
+            
+            for (let i = 0; i < randomNumber; i++) {
+                cy.wrap($items[i])
+            }
+        });
     }
-}
+};
 export default Inventory
